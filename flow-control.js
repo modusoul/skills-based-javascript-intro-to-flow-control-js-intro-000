f2e58c6d000974ae1713adaec5d8253b9fae8a17
antonio allen <modusoul@gmail.com>
2020-07-21 21:57:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,47 @@
+function basicTeenager(age)
+{
+  if (age>=13 && age<=19)
+  {
+    return "You are a teenager"
+  }
+}
 
+function teenager(age)
+{
+  if (age>= 13 && age<=19)
+{
+  return "You are a teenager";
+}
+else
+{
+  return "You are not a teenager";
+}}
+
+function ageChecker(age)
+{
+  if (age>=13 && age<=19)
+{
+  return "You are a teenager!";
+}
+  else if (age<=12)
+  {
+    return "You are a kid";
+  }
+else if (age>19)
+{
+  return "You are a grown up";
+}}
+
+
+function ternaryTeenager(age)
+{
+  return (age>=13 && age<=19)
+
+  "You are a teenager":
+
+  "You are not a teenager"
+}
+// }
 
 // function switchAge(age)
 //  {
@@ -49,7 +92,7 @@
 //   default:
 //
 //       return "You have an age";
-//   
+//   }
 //
 // }
 
