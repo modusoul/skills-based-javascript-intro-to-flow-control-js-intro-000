1babe0dba4a7f85a5542180541046f9be20d390d
antonio allen <modusoul@gmail.com>
2020-07-21 22:46:50 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,36 +1,36 @@
-function basicTeenager(age)
-{
-  if (age>=13 && age<=19)
-  {
-    return "You are a teenager"
-  }
-}
-
-function teenager(age)
-{
-  if (age>= 13 && age<=19)
-{
-  return "You are a teenager";
-}
-else
-{
-  return "You are not a teenager";
-}}
-
-function ageChecker(age)
-{
-  if (age>=13 && age<=19)
-{
-  return "You are a teenager!";
-}
-  else if (age<=12)
-  {
-    return "You are a kid";
-  }
-else if (age>19)
-{
-  return "You are a grown up";
-}}
+// function basicTeenager(age)
+// {
+//   if (age>=13 && age<=19)
+//   {
+//     return "You are a teenager"
+//   }
+// }
+//
+// function teenager(age)
+// {
+//   if (age>= 13 && age<=19)
+// {
+//   return "You are a teenager";
+// }
+// else
+// {
+//   return "You are not a teenager";
+// }}
+//
+// function ageChecker(age)
+// {
+//   if (age>=13 && age<=19)
+// {
+//   return "You are a teenager!";
+// }
+//   else if (age<=12)
+//   {
+//     return "You are a kid";
+//   }
+// else if (age>19)
+// {
+//   return "You are a grown up";
+// }}
 
 
 function ternaryTeenager(age)
